5d98b9d7bc745d17845798fb719f756ee93a63fc
kajal kumari <2005050100024kajal@gmail.com>
2025-01-24 10:43:54 +0530

    qw4

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col justify-center bg-amber-50">
+<div class="flex flex-col justify-center bg-amber-50 fixed w-full z-50">
     <h1 class="text-xl text-center p-1 text-black">🎉Order Now 🎉
         Apply 50% Discount Code</h1>
     <p class="text-center text-lg text-black">Free Dilevery : Free Shipping</p>    

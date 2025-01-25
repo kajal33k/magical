3dc74266789c0aff8f10942a15bafd12b0aaa818
kajal kumari <2005050100024kajal@gmail.com>
2025-01-25 15:46:23 +0530

    ades

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -1,4 +1,4 @@
-<div class="fixed top-0 w-full h-16 bg-[#88D040] text-white z-30 shadow-md  px-4">
+<div class="fixed top-0 w-full h-16 bg-[#88D040] text-white z-30 shadow-md">
     <div class="text-center">
         <h1 class="md:text-base text-sm font-semibold flex items-center justify-center text-wrap text-black">
         BIG BILLION SALE IS LIVE TODAY OFFER 50% OFF

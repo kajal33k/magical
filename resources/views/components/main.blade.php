d2da7a06b841fc7f40a54e093b58ad4a75aeba44
chandan <chandandigital6@gmail.com>
2025-01-30 13:54:56 +0530

    add fixel

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -22,7 +22,25 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css">
 
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
+<!-- Meta Pixel Code -->
+<!-- Meta Pixel Code -->
+<script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1258237308597358');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=1258237308597358&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Meta Pixel Code -->
+    <!-- End Meta Pixel Code -->
     </head>
 
     <body>
@@ -47,7 +65,7 @@
 
     <!-- JavaScript Libraries -->
     <!-- jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></>
 
 <!-- Owl Carousel CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css" integrity="sha512-9CW40OaH2wSLi1UbIwxZRwa5y9KNKIzP+i1VZCVNJzCMXM3yE4dGy5+KEW5OAtK40eWyH02uAGPxLepBaumKmw==" crossorigin="anonymous" referrerpolicy="no-referrer" />

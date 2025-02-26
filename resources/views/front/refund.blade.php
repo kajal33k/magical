45f54de91d311fbe9625cac07519b8b85c3b4edc
Kajal kumari <kajaldewan2023@gmail.com>
2025-02-26 11:15:34 +0530

    es5tf

--- resources/views/front/refund.blade.php
+++ resources/views/front/refund.blade.php
@@ -12,9 +12,7 @@
         <p class="text-gray-700 leading-relaxed mt-4">
             Cancellations will only be considered if the request is made within 1 day of placing the order. However, cancellation requests may not be entertained if the orders have been communicated to such sellers / merchant(s) listed on the Platform and they have initiated the process of shipping them, or the product is out for delivery. In such an event, you may choose to reject the product at the doorstep.
         </p>
-        <p class="text-gray-700 leading-relaxed mt-4">
-            MM ENTERPRISES does not accept cancellation requests for perishable items like flowers, eatables, etc. However, the refund / replacement can be made if the user establishes that the quality of the product delivered is not good.
-        </p>
+       
         <p class="text-gray-700 leading-relaxed mt-4">
             In case of receipt of damaged or defective items, please report to our customer service team. The request would be entertained once the seller/merchant listed on the Platform has checked and determined the same at its own end. This should be reported within 1 day of receipt of products. In case you feel that the product received is not as shown on the site or as per your expectations, you must bring it to the notice of our customer service within 1 day of receiving the product. The customer service team, after looking into your complaint, will take an appropriate decision.
         </p>
--- resources/views/front/shipping.blade.php
+++ resources/views/front/shipping.blade.php
@@ -10,6 +10,10 @@
 
       The orders for the user are shipped through registered domestic courier companies and/or speed post only. Orders are shipped within 3 days from the date of the order and/or payment or as per the delivery date agreed at the time of order confirmation and delivering of the shipment, subject to courier company / post office norms. Platform Owner shall not be liable for any delay in delivery by the courier company / postal authority. Delivery of all orders will be made to the address provided by the buyer at the time of purchase. Delivery of our services will be confirmed on your email ID as specified at the time of registration. If there are any shipping cost(s) levied by the seller or the Platform Owner (as the case be), the same is not refundable.
       </p>
+      <p class="text-lg font-semibold text-gray-700 bg-gray-100 px-4 py-2 rounded-md ">
+        Delivery Time: <span class="text-[#88D040] font-bold"> 7 to 10 Days</span>
+      </p>
+      
 </section>
 </div>    
 @endsection

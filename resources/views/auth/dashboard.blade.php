21400116cfc57f5fafb4e4a5806fff7a9c5f9be4
Jigyanshu <jigyanshu>
2025-03-01 17:59:22 +0530

    jnfnksf

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -5,15 +5,14 @@
         <div class="alert alert-danger">
             {{ session('error') }}
         </div>
-
     @elseif(session('success'))
         <div class="alert alert-primary">
             {{ session('success') }}
         </div>
     @endif
+    
     <section class="content">
         <div class="container-fluid">
-            <!-- Small boxes (Stat box) -->
             <div class="row">
                 <div class="col-lg-3 col-6">
                     <div class="small-box bg-info">
@@ -24,23 +23,25 @@
                         <div class="icon">
                             <i class="ion ion-bag"></i>
                         </div>
+                        <a href="{{route('orderRequest.index')}}" class="btn btn-danger w-100 mt-2">Get More Details</a>
                     </div>
                 </div>
                 
                 <div class="col-lg-3 col-6">
                     <div class="small-box bg-success">
                         <div class="inner">
-                            <h3>{{ $todayOrders}}</h3>
+                            <h3>{{ $todayOrders }}</h3>
                             <p>Today's Orders</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-stats-bars"></i>
                         </div>
+                        <a href="{{route('orderRequest.index')}}" class="btn btn-danger w-100 mt-2">Get More Details</a>
                     </div>
                 </div>
                 
                 @php
-                    $contact=\App\Models\Lead::count();
+                    $contact = \App\Models\Lead::count();
                 @endphp
                 <div class="col-lg-3 col-6">
                     <div class="small-box bg-warning">
@@ -51,47 +52,28 @@
                         <div class="icon">
                             <i class="ion ion-person-add"></i>
                         </div>
+                        <a href="{{ route('contact.index') }}" class="btn btn-danger w-100 mt-2">Get More Details</a>
                     </div>
                 </div>
-
-
-
-                
                 
                 @php
-                $counterVisitor = \App\Models\Visitor::count();
-            @endphp
-            
-            
+                    $counterVisitor = \App\Models\Visitor::count();
+                @endphp
                 <div class="col-lg-3 col-md-6 col-12">
                     <div class="card shadow-sm border-0 bg-warning text-white">
                         <div class="card-body d-flex align-items-center justify-content-between">
                             <div>
                                 <h3 class="mb-0">{{ $counterVisitor }}</h3>
                                 <p class="mb-0">Total Visitors</p>
-                               
                             </div>
                             <div class="icon">
                                 <i class="fas fa-users fa-3x"></i>
                             </div>
-                           
                         </div>
-                        <a href="{{ route('visitors.index') }}" class="btn btn-danger w-100">GetMoreDetails
-
-                        </a>
+                        <a href="{{ route('visitors.index') }}" class="btn btn-danger w-100 mt-2">Get More Details</a>
                     </div>
                 </div>
-            
-            
-
-                
-                
-                <!-- ./col -->
             </div>
-            <!-- /.row -->
-            <!-- Main row -->
-
-            <!-- /.row (main row) -->
-        </div><!-- /.container-fluid -->
+        </div>
     </section>
 @endsection

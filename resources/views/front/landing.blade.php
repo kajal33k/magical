e60a5e019ad49ef14c7e101e5e65782047cb9dfa
Kajal kumari <kajaldewan2023@gmail.com>
2025-03-11 16:24:27 +0530

    dts

--- resources/views/front/landing.blade.php
+++ resources/views/front/landing.blade.php
@@ -685,8 +685,8 @@
                 </form>
 
 
-                <!-- Include jQuery -->
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                {{-- <!-- Include jQuery -->
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
 
                 <script>
                     $(document).ready(function() {
